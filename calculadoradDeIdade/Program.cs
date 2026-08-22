9d60866420a84c8dcd27ee169425bb62375fbf8a
Igor Bruno <arquitetoigorbruno@gmail.com>
2026-08-22 19:13:44 -0300

    Commit inicial: verificador de idade e maioridade em .NET

---
+++ calculadoradDeIdade/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("DIgite o seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Olá {name}!");
+
+Console.WriteLine("Digite o ano que você nasceu: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2026 - year;
+Console.WriteLine($"Você tem {age} anos.");
+
+if (age > 17)
+{
+    Console.WriteLine("Você é maior de idade.");
+}
+else
+{
+    Console.WriteLine("Você é menor de idade.");
+}
